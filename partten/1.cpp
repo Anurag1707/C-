fdac0fc01f96e80772a4caf3c7ae70b101f59cb1
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-09-21 13:44:40 +0530

    c++

--- partten/1.cpp
+++ partten/1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int a=4;
+    for(int i=0;i<a;i++){
+        for(int j=i;j<a;j++){
+            cout<<"*";
+        }
+        cout<< endl;
+    }
 }
