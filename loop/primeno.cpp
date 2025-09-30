38c6af51c94012aa95594cdabb19b176bf970010
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-09-30 21:40:21 +0530

    C++

--- loop/primeno.cpp
+++ loop/primeno.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-for(int i=0;i<6;i++){
-    for(int j=)
+for(int i=0;i<3;i++){
+    for(int k=i;k>=0;k--){
+        cout<<" ";
+    }
+    for(int j=i;j<3;j++){
+        cout<<"*";
+    }
+    for(int p=i;p<2;p++){
+        cout<<"*";
+    
+    }
+    
+    cout << endl;
 }
 
 
--- partten/2.cpp
+++ partten/2.cpp
@@ -1,18 +1,19 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int a=3;
-//     for(int i=0;i<a;i++){
-//         for(int j=0;j<i;j++){
-//             cout<<" ";
+#include <iostream>
+using namespace std;
+int main(){
+    //djfswlnlsdf.
+    int a=3;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
 
-//         }
-//         for(int k=a;k>i;k--){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// }
+        }
+        for(int k=a;k>i;k--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
    
     //   ***
     //    **
