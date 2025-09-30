7ef81a063bb03783adb6e96ef73e63ed24aa6d34
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-09-30 20:20:23 +0530

    C++

--- partten/2.cpp
+++ partten/2.cpp
@@ -3,13 +3,17 @@
 // int main(){
 //     int a=3;
 //     for(int i=0;i<a;i++){
-//         for(int j=a-1;j>i;j--){
+//         for(int j=0;j<i;j++){
 //             cout<<" ";
 
 //         }
-//         for(int k=0;k<=i;k++){
+//         for(int k=a;k>i;k--){
 //             cout<<"*";
 //         }
 //         cout<<endl;
 //     }
 // }
+   
+    //   ***
+    //    **
+    //     *
---
+++ partten/3.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<" ";
+//         }
+//         for(int k=3;k>i;k--){
+//             cout<<"*";
+//         }
+//         for(int p=2;p>i;p--){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+//     // *****
+//     //  ***
+//     //   *
